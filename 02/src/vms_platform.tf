9d68708ce228b941ab247bb01ac509154db35e7c
SemikovaTV <129278233+SemikovaTV@users.noreply.github.com>
2023-12-14 21:27:34 +0300

    Update vms_platform.tf

--- 02/src/vms_platform.tf
+++ 02/src/vms_platform.tf
@@ -6,30 +6,6 @@ variable "vm_web_family" {
   description = "image family vm1"
 }
 
-variable "vm_web_platform"{
-  type        = string
-  default     = "standard-v1"
-  description = "platform_id vm1"
-}
-
-variable "vm_web_cores" {
-  type        = string
-  default     = "2"
-  description = "cores vCPU vm1"
-}
-
-variable "vm_web_memory"{
-  type        = string
-  default     = "1"
-  description = "vm memory vm1"
-}
-
-variable "vm_web_core_fraction" {
-  type        = string
-  default     = "5"
-  description = "core fraction vm1"
-}
-
 ###vm2
 variable "vm_db_family" {
   type        = string
@@ -43,23 +19,6 @@ variable "vm_db_platform"{
   description = "platform_id"
 }
 
-variable "vm_db_cores" {
-  type        = string
-  default     = "2"
-  description = "cores vCPU"
-}
-
-variable "vm_db_memory"{
-  type        = string
-  default     = "2"
-  description = "vm memory"
-}
-
-variable "vm_db_core_fraction" {
-  type        = string
-  default     = "20"
-  description = "core fraction"
-}
 
 ###for vms resources
 variable "vms_resources" {
